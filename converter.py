f9fe8b09c8d6c99eaa7038873dba2ce46608ce92
iam-saitama <aidapdiamond@gmail.com>
2024-11-20 02:31:53 +0500

    first

--- converter.py
+++ converter.py
@@ -6,6 +6,7 @@ bot = telebot.TeleBot('7526150677:AAHh7H12GFRwJr5ml7YU_duIxmG5Y7E13Bc')
 url = 'https://cbu.uz/ru/arkhiv-kursov-valyut/json/'
 USD = requests.get(url).json()[0]['Rate']
 EUR = requests.get(url).json()[1]['Rate']
+RUB = requests.get(url).json()[2]['Rate']
 
 
 @bot.message_handler(commands=['start'])
@@ -23,6 +24,9 @@ def text(message):
     elif message.text.title() == '€':
         bot.send_message(user_id, 'Введите сумму в UZS', reply_markup=telebot.types.ReplyKeyboardRemove())
         bot.register_next_step_handler(message, eur)
+    elif message.text.title() == '₽':
+        bot.send_message(user_id, 'Введите сумму в UZS', reply_markup=telebot.types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, rub)
     else:
         bot.send_message(user_id, 'Что-то пошло не так. Нажмите /start')
 
@@ -41,6 +45,14 @@ def eur(message):
     else:
         bot.send_message(user_id, 'Пишите только цифры!')
         bot.register_next_step_handler(message, eur)
+
+def rub(message):
+    user_id = message.from_user.id
+    if message.text.isnumeric():
+        bot.send_message(user_id, f'{int(message.text)/float(RUB)}')
+    else:
+        bot.send_message(user_id, 'Пишите только цифры!')
+        bot.register_next_step_handler(message, rub)
 
 
 bot.polling()
--- keyboard.py
+++ keyboard.py
@@ -4,6 +4,7 @@ def menu():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     usd = types.KeyboardButton('$')
     eur = types.KeyboardButton('€')
-    kb.add(usd, eur)
+    rub = types.KeyboardButton('₽')
+    kb.add(usd, eur, rub)
 
     return kb
